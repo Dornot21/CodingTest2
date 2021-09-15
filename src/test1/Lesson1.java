32f667869300b4cca816d2d71cdfb976c3c58a95
Dornot21 <dornot@kakao.com>
2021-09-15 22:43:59 +0900

    Test commit

---
+++ src/test1/Lesson1.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        String result = solution();
+        System.out.println(result);
+    }
+
+    public static String solution(){
+        return "11";
+    }
+}
